b114fc59914880d09c24a07c27bb5eb603802a3e
uodavid <uodavid@unicauca.edu.co>
2023-10-30 14:22:01 -0500

    feat: array methods

--- es6.js
+++ es6.js
@@ -1,4 +1,6 @@
 //retornos en una funcion
+
+
 function inicio(){ //una funcion mas grande llamada inicio 
 
     //funcion tradicional
@@ -27,6 +29,61 @@ function inicio(){ //una funcion mas grande llamada inicio
     console.log(result);
 }
 
-inicio();
+//inicio();
+
+//IIFE expresiones de funcion inmediatamente invocadas
+//cuando tenemos una funcion q se llame una vez pero que se llame cuando cargo la pagina
+
+(function(){
+    console.log("Hola mundo");
+})()
+
+
+//ARRAYS
+
+const baloto = [18, 28, 57, 12, 56, 87, 1, 28];
+console.log(baloto);
+
+//agregar un elemento al final del array 
+baloto.push(30);
+console.log(baloto);
+
+//para agregar al inicio se hace con la funcion UNSHIFT
+baloto.unshift(100);
+console.log(baloto);
+
+//eliminar un elemnto del array 
+
+baloto.pop();
+console.log(baloto);
+
+//eliminar el primer elemento del array
+baloto.shift();
+console.log(baloto);
+
+//si quiero que todos los elementos del arrays los multiplique por 2 
+
+//map retornamos el valor en una variable
+
+const array2 = baloto.map((item) => item = item * 2);
+console.log(array2);
+
+
+//for each no podemos retornar el valor en una variable
+
+baloto.forEach((el) => {
+    console.log(el);
+});
+
+
+
+
+//metodo filter filtra los elementos de un array que cumplen una condicion
+
+const filteredNumbers = baloto.filter((el) => el >= 30);
+console.log(filteredNumbers);
+
+
+
 
 
